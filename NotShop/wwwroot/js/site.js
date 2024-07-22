918edfb92810d98ef7faa444631472a92ef258da
olegsharkov <olegvamp@outlook.com>
2024-07-22 18:38:46 +0300

    add test bootstrap accardion

--- NotShop/wwwroot/js/site.js
+++ NotShop/wwwroot/js/site.js
@@ -1,5 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
+﻿$(".panel-heading").parent('.panel').hover(
+    function() {
+        $(this).children('.collapse').collapse('show');
+    }, function() {
+        $(this).children('.collapse').collapse('hide');
+    }
+);
